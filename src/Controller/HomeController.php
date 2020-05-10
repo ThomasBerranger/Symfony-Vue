230127dd8931c1e02587512b6b15ea1deec1570a
ThomasBerranger <tberranger@hotmail.fr>
2020-05-10 11:29:20 +0200

    global var, trending movies display

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct()
+    {
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
+        $client = HttpClient::create();
+
+        $response = $client->request('GET', 'https://api.themoviedb.org/3/trending/movie/week', [
+            'query' => [
+                'api_key' => $this->getParameter('app.tmdb.id'),
+            ],
+        ]);
+
+        $content = $response->toArray();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'trendingMovies' => $content['results'],
         ]);
     }
 }
